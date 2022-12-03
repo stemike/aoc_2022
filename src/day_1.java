87888a86a3c749e9763d7c7a09593c57b0fbc3c8
stemike <48892424+stemike@users.noreply.github.com>
2022-12-03 11:53:41 +0100

    Day 1 Part 2 multiple elves

--- src/day_1.java
+++ src/day_1.java
@@ -6,18 +6,26 @@ import java.util.Scanner;
 public class day_1 {
     public static void main(String[] args) throws IOException {
         String file = "src/data/d1_input.txt";
-        int max_calories = -1;
+        int n_elves = 3;
+        int[] max_calories = new int[n_elves];
 
         Scanner scanner = new Scanner(new File(file));
         scanner.useDelimiter("\n\n");
+
+        int placeholder;
         while (scanner.hasNext()) {
             int calorie_sum = Arrays.stream(scanner.next().split("\n")).mapToInt(Integer::parseInt).sum();
-            if (calorie_sum > max_calories) {
-                max_calories = calorie_sum;
+            for (int i = 0; i < n_elves; i++) {
+                if (calorie_sum > max_calories[i]) {
+                    placeholder = max_calories[i];
+                    max_calories[i] = calorie_sum;
+                    calorie_sum = placeholder;
+                }
             }
         }
+
         scanner.close();
-        System.out.println(max_calories);
+        System.out.println(Arrays.stream(max_calories).sum());
     }
 }
 
