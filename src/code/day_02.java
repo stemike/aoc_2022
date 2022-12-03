d193cce0a81a48930bd290a240ed9e9076621892
stemike <48892424+stemike@users.noreply.github.com>
2022-12-03 20:13:04 +0100

    Finished Day 2

--- src/code/day_02.java
+++ src/code/day_02.java
@@ -11,6 +11,7 @@ public class day_02 {
     // 1, 2, 3 Points for R, P, S
     public static void main(String[] args) throws IOException {
         String file_name = "src/data/d2_input.txt";
+        boolean part_two = true;
         int score = 0;
         int opponent_rock_char = 'A';
         int player_rock_char = 'X';
@@ -28,16 +29,32 @@ public class day_02 {
             round[0] -= opponent_rock_char;
             round[1] -= player_rock_char;
 
+            if(part_two){
+                round[1] = getPlayerMove(round[0], round[1]);
+            }
+
             score += getScoreForRound(round[0], round[1]);
         }
         scanner.close();
         System.out.println(score);
     }
 
+    private static int getPlayerMove(int opponent, int outcome){
+        // outcome values: 0 Lose, 1 Draw, 2 Win
+        if(outcome == 1){
+            return opponent;
+        } else if ((outcome == 0 && opponent == 1) || (outcome == 2 && opponent == 2)) {
+            return 0; // rock
+        } else if ((outcome == 0 && opponent == 2) || (outcome == 2 && opponent == 0)) {
+            return 1; // paper
+        } else{
+            return 2; // scissor
+        }
+    }
     private static int getScoreForRound(int opponent, int player) {
         int score = 0;
         // Win Scenarios
-        if (player > opponent || (player == 0 && opponent == 2)){
+        if ((player == 0 && opponent == 2) || (player == 1 && opponent == 0) || (player == 2 && opponent == 1)){
             score += 6;
         } else if (opponent == player) {
             score += 3;
